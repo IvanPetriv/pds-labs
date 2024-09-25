0a323e146da63cc3087cbb4b02575f1be80b2944
Ivan-Petriv <ivan.petriv.pz.2021@lpnu.ua>
2024-09-25 13:09:54 +0300

    Added saving hash to file

--- controller/md5_controller.py
+++ controller/md5_controller.py
@@ -1,4 +1,5 @@
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 from kivy.uix.widget import Widget
 from plyer import filechooser
 
@@ -9,19 +10,45 @@ Builder.load_file('view/md5_view.kv')
 
 
 class Md5Controller(Widget):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    """
+    Widget that takes a string argument and converts it to its hash form
+    using MD5 algorithm
+    """
+    normal_text: StringProperty | str = StringProperty("")
+    hashed_text: StringProperty | str = StringProperty("")
 
-
-    def open_file(self):
+    def open_file(self) -> None:
+        """
+        Opens the file manager that allows you to choose a text file,
+        after that reads and stores the contents of the chosen file
+        """
         path = filechooser.open_file(title="Pick a text file..", filters=["*.txt"])
-        print(path)
         with open(path[0], "r") as text_file:
-            self.ids.input_text.text = text_file.read()
+            self.normal_text = text_file.read()
+
+    def save_to_file(self) -> None:
+        """
+        Opens the file manager that allows you to choose a directory,
+        after that creates a file and stores the hash there
+        """
+        file_path = filechooser.save_file(title="Save Hash", filters=["*.txt"])
+
+        if file_path:
+            if isinstance(file_path, list):
+                file_path = file_path[0]
+            try:
+                with open(file_path, 'w') as file:
+                    file.write(self.hashed_text)
+                print(f"File saved at: {file_path}")
+            except IOError as e:
+                print(f"An error occurred while saving the file: {e}")
+        else:
+            print("Save operation cancelled.")
 
 
-    def generate_hash(self):
-        print(self.ids.input_text.text, self.ids.input_text)
-        result = Md5.md5(self.ids.input_text.text)
-        print(result)
-        self.ids.result_data.text = result
+    def generate_hash(self) -> None:
+        """
+        Generates an MD5 hash from the input text and outputs it
+        """
+        result = Md5.md5(self.normal_text)
+        self.hashed_text = result
